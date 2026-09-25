ea389c0588e198c1b1a6afd6ce1c2acd3fd2d4ec
Jose Carlos <josecarlos070896@gmail.com>
2026-09-25 19:09:26 +0200

    Arreglo de ejercicio 2.

--- ENTORNO CLIENTE/TEMA 2/EJ_ARRAYS/ejercicio02.js
+++ ENTORNO CLIENTE/TEMA 2/EJ_ARRAYS/ejercicio02.js
@@ -1,11 +1,12 @@
-
+//Añado el array.
 const numeros = [-4, -2 , -8, -1, -3, -6];
+// Creo las variables donde se acumularan los resultados
 let producto =1;
 let mayor = numeros[0];
 let suma = 0;
 let media = 0;
 
-
+//En este bucle for recorremos elemento a elemento y en el if si el numero actual es mayor que el encontrado se actualiza.
 for (let i = 0; i < numeros.length; i++) {
   producto = producto * numeros[i];
   suma = suma + numeros[i];
@@ -14,6 +15,8 @@ for (let i = 0; i < numeros.length; i++) {
   }
   
 }
-media = suma / numeros.length;
 
+// Hacemos la media
+media = suma / numeros.length;
+// Mostramos por consola
 console.log("Con:", numeros, "\nProducto:", producto, "\nMayor:", mayor, "\nMedia:", media);
